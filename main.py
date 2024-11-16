724ddac4e10949758736b7edc178af8b031d69b7
vedanshipanchal <vedanshipanchal@gmail.com>
2024-11-16 16:45:39 +0000

    Get the country code

---
+++ main.py
@@ -0,0 +1,9 @@
+country_code={'India' : '0091', 'Australia' : '0025', 'Portugal' : '00351'}
+
+print("country code for Portugal:")
+print(country_code.get('Portugal','Not found'))
+
+
+
+print("country code for China:")
+print(country_code.get('China','Not found'))
